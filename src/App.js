9ad102076022127eee6859063cb5d7fddf8c9b1d
Alexandre S. Soares <ale_santos.soares@hotmail.com>
2020-08-14 16:01:22 -0300

    chore: update app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import Deliveries from './Containers/Deliveries'
+import axios from 'axios'
 
-function App() {
+import { buildAddressSpec } from './utils/addressSpec'
+
+const url = (
+  process.env.REACT_APP_API_URL
+  || 'http://localhost:3003/api/deliveries'
+)
+
+const geoCodeUrl = address => (
+  `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=AIzaSyBcWpu_almbzwvYc8aIGLv0uW-952i2yA4&channel=88&token=4051`
+)
+
+const App = () => {
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const { data } = await axios.get(url)
+        setData(data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+    fetch()
+  }, [])
+
+  const geoCode = async (address) => {
+    const { data: { results } } = await axios.get(geoCodeUrl(address))
+    return buildAddressSpec(results[0])
+  }
+
+  const getDeliveries = async () => {
+    try {
+      const { data } = await axios.get(url)
+      setData(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleDeleteDelivery = async id => {
+    try {
+      await axios.delete(`${url}/${id}`)
+      getDeliveries()
+    } catch (error) {
+      console.log('error ao deletar')
+    }
+  }
+
+  const actionTable = {
+    control: row => handleDeleteDelivery(row._id),
+    field: 'Remove',
+    label: 'Remove',
+  }
+
+  const handleSubmit = async formData => {
+    try {
+      await axios.post(url, formData)
+      getDeliveries()
+      return true
+    } catch (error) {
+      console.log('error ao cadastrar')
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Deliveries
+      geoCode={geoCode}
+      onSubmit={handleSubmit}
+      actionTable={actionTable}
+      data={data}
+    />
+  )
 }
 
-export default App;
+export default App
